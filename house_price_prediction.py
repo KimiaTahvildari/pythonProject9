77370144b355b61926065652557099f22f6d4b84
KimiaTahvildari <130475904+KimiaTahvildari@users.noreply.github.com>
2024-09-10 10:43:49 +0300

    adim 1 finished

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -48,14 +48,46 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
     print(f'num_cols: {len(num_cols)}')
     print(f'cat_but_car: {len(cat_but_car)}')
     print(f'num_but_cat: {len(num_but_cat)}')
-
-    # cat_cols + num_cols + cat_but_car = değişken sayısı.
-    # num_but_cat cat_cols'un içerisinde zaten.
-    # dolayısıyla tüm şu 3 liste ile tüm değişkenler seçilmiş olacaktır: cat_cols + num_cols + cat_but_car
-    # num_but_cat sadece raporlama için verilmiştir.
-
     return cat_cols, cat_but_car, num_cols
 
 cat_cols, cat_but_car, num_cols = grab_col_names(df)
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
 
-#####work till 9/9/2024 need to continue on adimm 1
+for col in cat_cols:
+    target_summary_with_cat(df,"SalePrice",col)
+
+
+# Bağımlı değişkenin incelenmesi
+df["SalePrice"].hist(bins=100)
+plt.show()
+# Bağımlı değişkenin logaritmasının incelenmesi
+np.log1p(df['SalePrice']).hist(bins=50)
+plt.show()
+
+corr = df[num_cols].corr()
+corr
+
+# Korelasyonların gösterilmesi
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.70):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+high_correlated_cols(df, plot=False)
+
+
